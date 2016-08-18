9c5f5c7eae225749c6ad076f7afaffca53c388ef
Claire Houston <c.houston@kainos.com>
2016-08-18 15:23:02 +0100

    Salary class
    Added getters and setters to salary class

--- articuno/src/employee/Salary.java
+++ articuno/src/employee/Salary.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 public class Salary {
 	private int emp_num;
+	
 	private int salary;
 	private Date start_date;
 	private Date end_date;
@@ -27,4 +28,31 @@ public class Salary {
 	public int getSalary(){
 		return salary;
 	}
+	public int getEmp_num() {
+		return emp_num;
+	}
+
+	public void setEmp_num(int emp_num) {
+		this.emp_num = emp_num;
+	}
+
+	public Date getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+
+	public Date getEnd_date() {
+		return end_date;
+	}
+
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+				}
 }
